6e681c3a021d6ae06fca884e5f8362ddfdf43ad9
mystiick <polmickael3@gmail.com>
2019-09-16 00:03:27 +0200

    ajout des notif a lactionvation et desactivation dun utilisateur

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -20,163 +20,186 @@ use AppBundle\Constante\NotificationConstate as notif;
  */
 class UserController extends Controller
 {
-	CONST CHEF = 'Chef d\'équipe';
+    CONST CHEF = 'Chef d\'équipe';
 
-	/**
-	 * Lists all users entities.
-	 * @Route(path="/", name="user_index", methods={"GET", "POST"})
-	 * @return \Symfony\Component\HttpFoundation\Response
-	 * @Security("is_granted('ROLE_CHEF')")
-	 */
-	public function indexAction()
-	{
-		$em = $this->getDoctrine()->getManager();
-		$userActif = $this->getUser();
-		$userActifRole = $userActif->getFirstRole($userActif);
-		$listUsers = $em->getRepository(User::class)->findAll();
-		$userChefRole = $em->getRepository(User::class)->getUserByRole();
-		/* Opération ternaire permettant de lister que les chef et ouvrier si lutilisateur courant
-		 * Est un chef d'équipe
-		 * Sinon on affiche tous les utlilisateur pour le grade au dessus
-		*/
-		$users = $userActifRole === self::CHEF ? $userChefRole : $listUsers;
+    /**
+     * Lists all users entities.
+     * @Route(path="/", name="user_index", methods={"GET", "POST"})
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Security("is_granted('ROLE_CHEF')")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $userActif = $this->getUser();
+        $userActifRole = $userActif->getFirstRole($userActif);
+        $listUsers = $em->getRepository(User::class)->findAll();
+        $userChefRole = $em->getRepository(User::class)->getUserByRole();
+        /* Opération ternaire permettant de lister que les chef et ouvrier si lutilisateur courant
+         * Est un chef d'équipe
+         * Sinon on affiche tous les utlilisateur pour le grade au dessus
+        */
+        $users = $userActifRole === self::CHEF ? $userChefRole : $listUsers;
 
-		return $this->render('user/index.html.twig', array(
-			'users'     => $users,
-		));
-	}
+        return $this->render('user/index.html.twig', array(
+            'users' => $users,
+        ));
+    }
 
-	/**
-	 * Creates a new user entity.
-	 * @Route(path="/new", name="user_new", methods={"GET", "POST"})
-	 * @param Request $request
-	 * @Security("is_granted('ROLE_CHEF')")
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function newAction(Request $request)
-	{
-		$user = new User();
-		$form = $this->createForm(UserType::class, $user);
-		$form->handleRequest($request);
+    /**
+     * Creates a new user entity.
+     * @Route(path="/new", name="user_new", methods={"GET", "POST"})
+     * @param Request $request
+     * @Security("is_granted('ROLE_CHEF')")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function newAction(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
-		if ($form->isSubmitted() && $form->isValid()) {
-			$em = $this->getDoctrine()->getManager();
-			$user->setEnabled(true);
-			$em->persist($user);
-			$em->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $user->setEnabled(true);
+            $em->persist($user);
+            $em->flush();
 
-			$this->addFlash(
-				notif::SUCCESS,
-				sprintf('L\'utilisateur %s à bien été créer !', $user)
-			);
+            $this->addFlash(
+                notif::SUCCESS,
+                sprintf('L\'utilisateur %s à bien été créer !', $user)
+            );
 
-			/*, array('id' => $bien->getId())*/
-			return $this->redirectToRoute('user_index');
-		}
+            /*, array('id' => $bien->getId())*/
+            return $this->redirectToRoute('user_index');
+        }
 
-		return $this->render('user/new.html.twig', array(
-			'user' => $user,
-			'form' => $form->createView(),
-		));
-	}
+        return $this->render('user/new.html.twig', array(
+            'user' => $user,
+            'form' => $form->createView(),
+        ));
+    }
 
-	/**
-	 * Displays a form to edit an existing user entity.
-	 * @Route(path="/{id}/edit", name="user_edit", methods={"GET", "POST"})
-	 * @param Request $request
-	 * @param User $user
-	 * @Security("is_granted('ROLE_CHEF')")
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function editAction(Request $request, User $user)
-	{
-		$editForm = $this->createForm(EditUserType::class, $user);
-		$editForm->handleRequest($request);
+    /**
+     * Displays a form to edit an existing user entity.
+     * @Route(path="/{id}/edit", name="user_edit", methods={"GET", "POST"})
+     * @param Request $request
+     * @param User $user
+     * @Security("is_granted('ROLE_CHEF')")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction(Request $request, User $user)
+    {
+        $editForm = $this->createForm(EditUserType::class, $user);
+        $editForm->handleRequest($request);
 
-		if ($editForm->isSubmitted() && $editForm->isValid()) {
-			$newRole = [$request->request->get('appbundle_user')['role']];
-			$user->removeRole($user->getFirstRole($user));
-			$user->setRoles($newRole);
-			$this->getDoctrine()->getManager()->flush();
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $newRole = [$request->request->get('appbundle_user')['role']];
+            $user->removeRole($user->getFirstRole($user));
+            $user->setRoles($newRole);
+            $this->getDoctrine()->getManager()->flush();
 
-			$this->addFlash(
-				notif::INFO,
-				sprintf('Les changements concernant l\'utilisateur %s ont bien été pris en compte', $user)
-			);
+            $this->addFlash(
+                notif::INFO,
+                sprintf('Les changements concernant l\'utilisateur %s ont bien été pris en compte', $user)
+            );
 
-			return $this->redirectToRoute('user_index');
-		}
+            return $this->redirectToRoute('user_index');
+        }
 
-		return $this->render('user/edit.html.twig', array(
-			'user' => $user,
-			'edit_form' => $editForm->createView(),
-		));
-	}
-
-
-	/**
-	 * Displays a form to edit password an existing user entity.
-	 * @Route(path="/{id}/edit-password", name="user_password_edit", methods={"GET", "POST"})
-	 * @Security("is_granted('ROLE_CHEF')")
-	 * @param Request $request
-	 * @param User $user
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-	 */
-	public function editPasswordAction(Request $request, User $user)
-	{
-		$form = $this->createForm(EditUserPasswordType::class, $user);
-		$form->handleRequest($request);
-
-		if ($form->isSubmitted() && $form->isValid()) {
-			$em = $this->getDoctrine()->getManager();
-			$userManager = $this->container->get('fos_user.user_manager');
-			$userManager->updatePassword($user);
-			$em->flush();
-
-			$this->addFlash(
-				notif::WARNING,
-				sprintf('Le mot de passe de l\utilisateur %s à bien été modifé', $user)
-			);
-
-			return $this->redirectToRoute('user_index');
-		}
-
-		return $this->render('user/edit-password.html.twig', array(
-			'user' => $user,
-			'edit_form' => $form->createView(),
-		));
-	}
+        return $this->render('user/edit.html.twig', array(
+            'user'      => $user,
+            'edit_form' => $editForm->createView(),
+        ));
+    }
 
 
-	/**
-	 * Deletes a user entity.
-	 * @Route(path="/{id}", name="user_delete", methods={"GET"})
-	 * @param Request $request
-	 * @param User $user
-	 * @Security("is_granted('ROLE_DIRECTEUR')")
-	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
-	 */
-	public function deleteAction(Request $request, User $user)
-	{
-		$em = $this->getDoctrine()->getManager();
-		$em->remove($user);
-		$em->flush();
+    /**
+     * Displays a form to edit password an existing user entity.
+     * @Route(path="/{id}/edit-password", name="user_password_edit", methods={"GET", "POST"})
+     * @Security("is_granted('ROLE_CHEF')")
+     * @param Request $request
+     * @param User $user
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editPasswordAction(Request $request, User $user)
+    {
+        $form = $this->createForm(EditUserPasswordType::class, $user);
+        $form->handleRequest($request);
 
-		$this->addFlash(
-			notif::DANGER,
-			sprintf('L\'utilisateur %s à bien été supprimé', $user)
-		);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $userManager = $this->container->get('fos_user.user_manager');
+            $userManager->updatePassword($user);
+            $em->flush();
 
-		return $this->redirectToRoute('user_index');
-	}
+            $this->addFlash(
+                notif::WARNING,
+                sprintf('Le mot de passe de l\utilisateur %s à bien été modifé', $user)
+            );
 
-	/**
-	 * @Route(path="/ajax/edit/{id}", name="user_edit_ajax", methods={"POST"})
-	 * @Security("is_granted('ROLE_DIRECTEUR')")
-	 */
-	public function editEnabledAjax()
-	{
+            return $this->redirectToRoute('user_index');
+        }
 
-	}
+        return $this->render('user/edit-password.html.twig', array(
+            'user'      => $user,
+            'edit_form' => $form->createView(),
+        ));
+    }
+
+
+    /**
+     * Deletes a user entity.
+     * @Route(path="/{id}", name="user_delete", methods={"GET"})
+     * @param Request $request
+     * @param User $user
+     * @Security("is_granted('ROLE_DIRECTEUR')")
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function deleteAction(Request $request, User $user)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($user);
+        $em->flush();
+
+        $this->addFlash(
+            notif::DANGER,
+            sprintf('L\'utilisateur %s à bien été supprimé', $user)
+        );
+
+        return $this->redirectToRoute('user_index');
+    }
+
+    /**
+     * @Route(path="/ajax/edit/{id}", name="user_edit_ajax", methods={"POST"})
+     * @Security("is_granted('ROLE_DIRECTEUR')")
+     * @param Request $request
+     */
+    public function editEnabledAjax(Request $request)
+    {
+
+        if ($request->isXmlHttpRequest()) {
+            $em = $this->getDoctrine()->getManager();
+            $tab = $request->get('donnees');
+            $enabled = (int)$tab['enabled'] === 0 ? false : true;
+            $phraseSuccess = $enabled === true ? 'activé' : 'désactiver';
+            $tabUser = $tab['utilisateur'];
+            $user = $em->getRepository(User::class)->find($tabUser);
+
+            $user->setEnabled($enabled);
+            $em->persist($user);
+            $em->flush();
+
+            echo json_encode([
+                "label"   => "success",
+                "success" => sprintf('L\'utilisateur %s à bien été %s', $user, $phraseSuccess),
+            ]);
+            header('Content-Type: application/json');
+            http_response_code(200);
+            die;
+        }
+
+
+    }
 
 }
